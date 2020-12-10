a720737d37b4bc8d7f7e8661ab27f6e97f93b1f7
Lucia Cerchie <luciacerchie@gmail.com>
2020-12-10 12:59:45 -0700

    added routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom';
 import Eat from './Eat'
 import Run from './Run'
 import Home from './Home'
@@ -6,9 +8,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
+      <Route exact path="/">
       <Home/>
+      </Route>
+      <Route exact path="/eat">
       <Eat/>
+      </Route>
+      <Route exact path="/run">
       <Run/>
+      </Route>
+      </BrowserRouter>
     </div>
   );
 }
